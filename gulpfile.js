7de8d2431bb05004795dd0fe7828582fd220dcff
mliq <liquori@gmail.com>
2015-06-26 15:27:38 -0500

    Sass working

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    webserver = require('gulp-webserver');
+    webserver = require('gulp-webserver'),
+    sass = require('gulp-sass');
 
 gulp.task('js', function(){
     gulp.src('builds/development/js/**/*');
@@ -10,13 +11,20 @@ gulp.task('html', function(){
     gulp.src('builds/development/*.html');
 });
 
-gulp.task('css', function(){
-    gulp.src('builds/development/css/*.css');
+//gulp.task('css', function(){
+//    gulp.src('builds/development/css/*.css');
+//});
+
+gulp.task('sass', function(){
+    gulp.src('builds/development/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./builds/development/css/'));
 });
 
 gulp.task('watch', function(){
    gulp.watch('builds/development/js/**/*', ['js']);
-   gulp.watch('builds/development/css/*.css', ['css']);
+   //gulp.watch('builds/development/css/*.css', ['css']);
+   gulp.watch('builds/development/scss/*.scss', ['scss']);
    gulp.watch('builds/development/*.html', ['html']);
 });
 
@@ -28,4 +36,4 @@ gulp.task('webserver', function(){
        }));
 });
 
-gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
+gulp.task('default', ['watch', 'html', 'js', 'sass', 'webserver']);
